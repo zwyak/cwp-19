6ad97f5889d144ddb6ee749a7f24865b5dd9a0bf
zwyak <vlad.senyuk97@gmail.com>
2020-03-09 09:45:47 +0300

    Added signup

--- validator.js
+++ validator.js
@@ -5,6 +5,16 @@ const schemas = {
     login: Joi.string(),
     password: Joi.string(),
   }),
+
+  '/signup': Joi.object().keys({
+    login: Joi.string(),
+    password: Joi.string().min(10),
+    email: Joi.string().email().optional(),
+    inviteBy: Joi.string().optional(),
+    birth: Joi.date().max('1-1-1999'),
+    sex: Joi.string().valid('male', 'female'),
+    agreedWithTerms: Joi.boolean().valid(true)
+  }),
 };
 
 exports.check = function (schema, body) {
